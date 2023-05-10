87a5e6297eb903b4a34b009b2e63c9b483d49b4f
MrJinkyu <rlawlsrb167@naver.com>
2023-05-10 14:58:52 +0900

    비디오 카드 기능 구현 youtube open api

---
+++ .history/src/api/Youtube_20230510144146.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+export default class Youtube{
+    constructor(){
+        this.httpClient = axios.create({
+            baseURL:'',
+            params:{key}
+        })
+    }
+    async search(keyword){
+        return keyword ? this.#searchByKeyword(keyword) : this.#mostPopular();
+    }
+
+    async #searchByKeyword(){
+        return axios
+            .get('/videos/search.json')
+            .then((res) => res.data.items)
+            .then((items) => items.map((item) => ({...item, id: item.id.videoId})))
+    }
+
+    async #mostPopular(){
+        return axios
+            .get('/videos/popular.json')
+            .then((res) => res.data.items)
+    }
+}
